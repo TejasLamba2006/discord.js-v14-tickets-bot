1c154b8cba931a5ee8425efbac12a933c51c4789
Replit user <>
2023-04-17 21:05:29 -0700

    added database options, quick.db and mongo(quickmongo and mongoose)

--- botconfig/config.js
+++ botconfig/config.js
@@ -1,9 +1,12 @@
 module.exports = {
   "token": process.env.TOKEN,
+  "dbType": "", //quick.db OR MONGO
+  "MONGOtype": "", //quickmongo OR MONGOOSE
+  "MongoURL": "", //if dbType = MONGO, this is required else skip
   "prefix": "op",
   "loadSlashsGlobal": true,
   "dirSetup": [{
     "Folder": "Info", "CmdName": "info",
     "CmdDescription": "Grant specific Information about something!"
-}]
+  }]
 }
--- index.js
+++ index.js
@@ -1,33 +1,71 @@
+require('dotenv').config();
 const Discord = require("discord.js");
 const config = require(`./botconfig/config.js`);
 const settings = require(`./botconfig/settings.js`);
 const colors = require("colors");
 const client = new Discord.Client({
-    fetchAllMembers: false,
-    shards: "auto",
-    allowedMentions: {
-      parse: [ ],
-      repliedUser: false,
+  fetchAllMembers: false,
+  shards: "auto",
+  allowedMentions: {
+    parse: [],
+    repliedUser: false,
+  },
+  failIfNotExists: false,
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildIntegrations,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent,
+    Discord.GatewayIntentBits.GuildMessages
+  ],
+  presence: {
+    activity: {
+      name: `+help | Rainbow Studios`,
+      type: "PLAYING",
     },
-    failIfNotExists: false,
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: [
-        Discord.GatewayIntentBits.Guilds,
-         Discord.GatewayIntentBits.GuildMembers,
-         Discord.GatewayIntentBits.GuildIntegrations,
-        Discord.GatewayIntentBits.GuildVoiceStates,
-        Discord.GatewayIntentBits.MessageContent,
-      Discord.GatewayIntentBits.GuildMessages
-    ],
-    presence: {
-      activity: {
-        name: `+help | Rainbow Studios`,
-        type: "PLAYING",
-      },
-      status: "online"
-    }
+    status: "online"
+  }
 });
+//DataBase stuff
+if (config.dbType === "quick.db") {
+  const { QuickDB } = require("quick.db");
+  const db = new QuickDB();
+  client.db = db
+} else if (config.dbType === "MONGO") {
+  if (config.MONGOtype === "quickmongo") {
+    const { Database } = require('quickmongo')
+    const db = new Database(config.MongoURL);
+    main().catch(err => console.log(err));
+    db.on("ready", () => {
+      console.log("MongoDB connected!");
+    });
+    async function main() {
+      await db.connect();
+    }
+client.db = db
+  } else if (config.MONGOtype === "MONGOOSE") {
+    const mongoose = require('mongoose');
 
+    main().catch(err => console.log(err));
+
+    async function main() {
+      await mongoose.connect(config.MongoURL);
+    }
+    return new Promise((res) => {
+      mongo.connection.once("connected", () => {
+        console.log("MongoDB connected!");
+        res();
+      });
+    });
+  } else {
+    console.log('Invalid MongoType, only quickmongo and MONGOOSE Accepted')
+    process.exit(1)
+  }
+} else {
+  console.log('Invalid dbType OR No dbType given, Running without DataBase')
+}
 
 //Define some Global Collections
 client.commands = new Discord.Collection();
@@ -43,11 +81,11 @@ client.setMaxListeners(100); require('events').defaultMaxListeners = 100;
 
 //Require the Handlers Add the antiCrash file too, if its enabled
 ["events", "slashCommands", settings.antiCrash ? "antiCrash" : null]
-    .filter(Boolean)
-    .forEach(h => {
-        require(`./handlers/${h}`)(client);
-    })
+  .filter(Boolean)
+  .forEach(h => {
+    require(`./handlers/${h}`)(client);
+  })
 //Start the Bot
-client.login(config.token) 
+client.login(config.token)
 //End of the File
 
